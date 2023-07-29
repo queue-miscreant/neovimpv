f57b762684b85501df52d0e1fb059602ba056b5a
queue-miscreant <cubebert549@gmail.com>
2023-07-29 04:22:56 -0500

    separated MpvInstance into mpv, moved MpvProtocol to protocol

--- rplugin/python3/neovimpv/__init__.py
+++ rplugin/python3/neovimpv/__init__.py
@@ -6,23 +6,12 @@ import os.path
 import asyncio
 import json
 import re
-
 import logging
-import pynvim
-import sys
 
-sys.path.append(os.path.dirname(__file__))
-from neovimpv import mpv
+import pynvim
+from neovimpv.mpv import MpvInstance
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-
-# Relevant highlights:
-# "NonText" (dark comments)
-# "LineNr"     (comments)
-# "Title"      (functions)
-# "Directory"  (functions)
-# "Conceal"    (self)
 
 # the most confusing regex possible: [group1](group2)
 MARKDOWN_LINK = re.compile(r"\[([^\[\]]*)\]\(([^()]*)\)")
@@ -33,140 +22,6 @@ def try_json(arg):
         return json.loads(arg)
     except:
         return arg
-
-def sexagesimalize(number):
-    '''Convert a number to decimal-coded sexagesimal (i.e., clock format)'''
-    seconds = int(number)
-    minutes = seconds // 60
-    hours = minutes // 60
-    if hours:
-        return f"{(hours % 60):0{2}}:{(minutes % 60):0{2}}:{(seconds % 60):0{2}}"
-    else:
-        return f"{(minutes % 60):0{2}}:{(seconds % 60):0{2}}"
-
-EXTMARK_LOADING = {
-    "virt_text": [["[ ... ]", "Whitespace"]],
-    "virt_text_pos": "eol",
-}
-
-class MpvInstance:
-    '''
-    An instance of MPV which is aware of the nvim plugin.
-    Await `spawn` to create the subprocess and begin communication.
-    '''
-    # TODO configurable
-    MPV_ARGS = ["--no-video"]
-    def __init__(self, plugin, buffer, line):
-        self.protocol = None
-
-        self.plugin = plugin
-        self.buffer = buffer
-        self.id = plugin.live_extmark(buffer, EXTMARK_LOADING, line)
-
-    # TODO this should be configurable
-    def format_data(self):
-        paused = [" || ", "Conceal"] if self.protocol.data.get("pause") \
-            else [" |> ", "Title"]
-        position = sexagesimalize(self.protocol.data.get("playback-time", 0))
-        duration = sexagesimalize(self.protocol.data.get("duration", 0))
-        show_loop = ""
-        if (loop := self.protocol.data.get("loop")) == "inf":
-            show_loop = "∞"
-        elif loop:
-            show_loop = str(loop)
-
-        return [i for i in [
-            ["[", "LineNr"],
-            paused,
-            [f"{position} ", "Conceal"],
-            ["/ ", "LineNr"],
-            [f"{duration} ", "Conceal"],
-            [f"({show_loop}) ", "LineNr"] if show_loop else None,
-            ["]", "LineNr"],
-        ] if i]
-
-    def toggle_pause(self):
-        self.protocol.set_property("pause", not self.protocol.data.get("pause"), update=False)
-
-    def draw_update(self):
-        display = {
-            "id": self.id,
-            "virt_text": self.format_data(),
-            "virt_text_pos": "eol",
-        }
-
-        self.plugin.nvim.async_call(
-            self.plugin.live_extmark,
-            self.buffer.number,
-            display
-        )
-
-    def write_as_markdown(self, arg):
-        media_title = self.protocol.data.get("media-title")
-        filename = self.protocol.data.get("filename")
-        if media_title == filename:
-            return
-
-        self.plugin.nvim.async_call(
-            self.plugin.write_line_of_extmark,
-            self.buffer,
-            self.id,
-            [f"[{media_title.replace('[', '(').replace(']',')')}]({arg})"],
-        )
-
-    async def spawn(self, arg, timeout_duration=1, has_markdown=True):
-        '''
-        Spawn subprocess and wait `timeout_duration` seconds for error output.
-        If the connection is successful, the instance's `protocol` member will be set
-        to an MpvProtocol for IPC.
-        '''
-        ipc_path = os.path.join(self.plugin.mpv_socket_dir, f"{self.id}")
-
-        try:
-            process, protocol = await mpv.create_mpv(
-                self.MPV_ARGS,
-                ipc_path,
-                read_timeout=timeout_duration,
-                loop=self.plugin.nvim.loop
-            )
-            self.protocol = protocol
-
-            protocol.send_command("loadfile", os.path.expanduser(arg))
-            protocol.add_event("property-change", lambda _, __: self.draw_update())
-            if not has_markdown:
-                protocol.add_event("playback-restart", lambda _, __: self.write_as_markdown(arg))
-            protocol.add_event("error", lambda _, err: self._show_error(err))
-            protocol.add_event("end-file", lambda _, arg: self._on_end_file(arg))
-            protocol.add_event("file-loaded", lambda _, __: self.preamble())
-            protocol.add_event("close", lambda _, __: self.close())
-        except mpv.MpvError as e:
-            self.plugin.show_error(e.args[0])
-            if self.id is not None:
-                self.close()
-
-    def _show_error(self, err):
-        additional_info = ""
-        if (property_name := err.get("property-name")) is not None:
-            additional_info = f" to request for property '{property_name}'"
-
-        self.plugin.show_error(
-            f"MPV responded '{err.get('error')}'{additional_info}",
-        )
-
-    def _on_end_file(self, arg):
-        if arg.get("reason") == "error" and (error := arg.get("file_error")):
-            self.plugin.show_error(f"File ended: {error}")
-
-    def preamble(self):
-        self.protocol.observe_property("playback-time")
-        self.protocol.observe_property("pause")
-        self.protocol.observe_property("loop")
-        self.protocol.get_property("duration")
-        self.protocol.get_property("filename")
-        self.protocol.get_property("media-title")
-
-    def close(self):
-        self.plugin.nvim.async_call(self.plugin.remove_mpv_instance, self)
 
 @pynvim.plugin
 class NeoviMPV:
--- rplugin/python3/neovimpv/mpv.py
+++ rplugin/python3/neovimpv/mpv.py
@@ -1,195 +1,145 @@
-from subprocess import PIPE
-import asyncio
-import json
-import logging
+import os.path
 
-log = logging.getLogger(__name__)
+from neovimpv.protocol import create_mpv, MpvError
 
-class MpvError(Exception):
-    pass
+# Relevant highlights:
+# "NonText" (dark comments)
+# "LineNr"     (comments)
+# "Title"      (functions)
+# "Directory"  (functions)
+# "Conceal"    (self)
 
-class MpvProtocol(asyncio.Protocol):
-    '''
-    Protocol and storage for interacting with a mpv instance's IPC.
-    Supports event callbacks with signature (protocol, data) which can be added with `add_event`.
-    '''
-    SET = 0
-    GET = 1
-    def __init__(self):
-        self.transport = None
-        self.data = {}
+EXTMARK_LOADING = {
+    "virt_text": [["[ ... ]", "Whitespace"]],
+    "virt_text_pos": "eol",
+}
 
-        self._properties = {}
-        self._reverse_properties = {}
-        self._last_property = 20
-
-        self._event_handlers = {}
-        self._waiting_properties = {}
-        self.add_event("property-change", lambda _, data: self._property_change(data))
-
-    def _property_id(self, property_name):
-        '''Keep records of which properties we've sent before and decided on an ID for.'''
-        prop_id = self._properties.get(property_name, self._last_property)
-        if prop_id == self._last_property:
-            self._properties[property_name] = prop_id
-            self._reverse_properties[prop_id] = property_name
-            self._last_property += 1
-        return prop_id
-
-    def add_event(self, event_name, func):
-        '''
-        Add event handler. All MPV event names are valid, as are "connected", "close", and "error"
-        '''
-        if event_name not in self._event_handlers:
-            self._event_handlers[event_name] = []
-        self._event_handlers[event_name].append(func)
-        return func
-
-    def _try_handle_event(self, event_name, json_data):
-        '''Internal function for calling all event handlers for a given `event_name`'''
-        for handler in self._event_handlers.get(event_name, []):
-            handler(self, json_data)
-        if event_name != "property-change":
-            log.debug(f"Received event '{event_name}': {json_data}")
-
-    def connection_made(self, transport):
-        '''Process communication initiated. Save transport and send connected event.'''
-        self.transport = transport
-        self._try_handle_event("connected", {})
-
-    def data_received(self, data):
-        '''Split out received data into individual JSONs and send to storage'''
-        for datum in data.split(b"\n"):
-            if not datum.rstrip(): continue
-            datum = json.loads(datum)
-            request_id = datum.get("request_id")
-
-            if datum.get("error") not in ("success", None):
-                # reverse lookup the property name for convenience
-                if (property_name := self._reverse_properties.get(request_id)) is not None:
-                    datum.update({"property-name": property_name})
-                self._try_handle_event("error", datum)
-                continue
-            elif (event_name := datum.get("event")) is not None:
-                self._try_handle_event(event_name, datum)
-                continue
-            elif request_id is not None and request_id in self._reverse_properties:
-                # reverse lookup the property name for convenience
-                property_name = self._reverse_properties[request_id]
-                self.data[property_name] = datum.get("data")
-                log.debug(f"Got property {property_name}: {datum}")
-            elif request_id is not None and request_id in self._waiting_properties:
-                type, property_name, future = self._waiting_properties[request_id]
-                if type == self.GET:
-                    self.data[property_name] = datum.get("data")
-                    log.debug(f"Got awaited property {property_name}: {datum}")
-                    future.set_result(datum.get("data"))
-                elif type == self.SET:
-                    self.data[property_name] = future
-                    log.debug(f"Successfully set {property_name} to {datum}")
-            else:
-                log.debug(f"Unknown data received from MPV: {datum}")
-
-    def connection_lost(self, exc):
-        '''Process communication closed. Call close event.'''
-        self._try_handle_event("close", {})
-
-    def send_command(self, *args, request_id=0):
-        '''Write a command to the socket'''
-        command = {
-            "command": args,
-            "request_id": request_id,
-        }
-        log.debug(f"Sent command {command}")
-        self.transport.write((json.dumps(command) + "\n").encode())
-
-    def get_property(self, property_name, request_id=None):
-        '''
-        Send a command to retrieve a property from the MPV instance.
-        Note that this does NOT return the property!
-        '''
-        if request_id is None:
-            request_id = self._property_id(property_name)
-        self.send_command(
-            "get_property",
-            property_name,
-            request_id=request_id,
-        )
-
-    def set_property(self, property_name, value, update=True):
-        '''Send a command to set a property on the MPV instance.'''
-        if not update:
-            self.send_command("set_property", property_name, value)
-            return
-        self._waiting_properties[self._last_property] = (self.SET, property_name, value)
-
-        self.send_command("set_property", property_name, value, request_id=self._last_property)
-        self._last_property += 1
-
-    async def wait_property(self, property_name):
-        future = asyncio.get_event_loop().create_future()
-        self._waiting_properties[self._last_property] = (self.GET, property_name, future)
-
-        self.get_property(property_name, request_id=self._last_property)
-        self._last_property += 1
-        return await future
-
-    def observe_property(self, property_name):
-        '''
-        Send a command to observe a property from the MPV instance.
-        The value in self.data will be updated on "property-change" events.
-        '''
-        self.send_command(
-            "observe_property",
-            self._property_id(property_name),
-            property_name,
-        )
-
-    def _property_change(self, json_data):
-        '''Handler for MPV "property-change" events.'''
-        property_name = self._reverse_properties.get(json_data.get("id"))
-        data = json_data.get("data")
-        if property_name is not None and data is not None:
-            self.data[property_name] = data
-
-
-async def create_mpv(mpv_args, ipc_path, read_timeout=1, loop=None):
-    '''
-    Create an instance of mpv which uses MpvProtocol for IPC at the UNIX path `ipc_path`
-    Returns tuple of asyncio Process and MpvProtocol in use.
-    '''
-    if loop is None:
-        loop = asyncio.get_event_loop()
-
-    process = await asyncio.create_subprocess_exec(
-        "mpv",
-        *mpv_args,
-        f"--input-ipc-server={ipc_path}",
-        "--idle=once",
-        stdout=PIPE,
-    )
-
-    # timeout a read from the subprocess's stdout (for errors)
-    read_task = asyncio.create_task(process.stdout.read())
-    done, pending = await asyncio.wait(
-        [read_task],
-        timeout=read_timeout
-    )
-    if done:
-        error = read_task.result()
-        raise MpvError(error)
+def sexagesimalize(number):
+    '''Convert a number to decimal-coded sexagesimal (i.e., clock format)'''
+    seconds = int(number)
+    minutes = seconds // 60
+    hours = minutes // 60
+    if hours:
+        return f"{(hours % 60):0{2}}:{(minutes % 60):0{2}}:{(seconds % 60):0{2}}"
     else:
-        read_task.cancel()
-        try:
-            await read_task
-        except asyncio.CancelledError:
-            pass
+        return f"{(minutes % 60):0{2}}:{(seconds % 60):0{2}}"
 
-    try:
-        _, protocol = await loop.create_unix_connection(
-            MpvProtocol,
-            path=ipc_path,
+class MpvInstance:
+    '''
+    An instance of MPV which is aware of the nvim plugin.
+    Await `spawn` to create the subprocess and begin communication.
+    '''
+    # TODO configurable
+    MPV_ARGS = ["--no-video"]
+    def __init__(self, plugin, buffer, line):
+        self.protocol = None
+
+        self.plugin = plugin
+        self.buffer = buffer
+        self.id = plugin.live_extmark(buffer, EXTMARK_LOADING, line)
+
+    # TODO this should be configurable
+    def format_data(self):
+        paused = [" || ", "Conceal"] if self.protocol.data.get("pause") \
+            else [" |> ", "Title"]
+        position = sexagesimalize(self.protocol.data.get("playback-time", 0))
+        duration = sexagesimalize(self.protocol.data.get("duration", 0))
+        show_loop = ""
+        if (loop := self.protocol.data.get("loop")) == "inf":
+            show_loop = "∞"
+        elif loop:
+            show_loop = str(loop)
+
+        return [i for i in [
+            ["[", "LineNr"],
+            paused,
+            [f"{position} ", "Conceal"],
+            ["/ ", "LineNr"],
+            [f"{duration} ", "Conceal"],
+            [f"({show_loop}) ", "LineNr"] if show_loop else None,
+            ["]", "LineNr"],
+        ] if i]
+
+    def toggle_pause(self):
+        self.protocol.set_property("pause", not self.protocol.data.get("pause"), update=False)
+
+    def draw_update(self):
+        display = {
+            "id": self.id,
+            "virt_text": self.format_data(),
+            "virt_text_pos": "eol",
+        }
+
+        self.plugin.nvim.async_call(
+            self.plugin.live_extmark,
+            self.buffer.number,
+            display
         )
-        return process, protocol
-    except ConnectionRefusedError as e:
-        raise MpvError("Could not connect to MPV!") from e
+
+    def write_as_markdown(self, arg):
+        media_title = self.protocol.data.get("media-title")
+        filename = self.protocol.data.get("filename")
+        if media_title == filename:
+            return
+
+        self.plugin.nvim.async_call(
+            self.plugin.write_line_of_extmark,
+            self.buffer,
+            self.id,
+            [f"[{media_title.replace('[', '(').replace(']',')')}]({arg})"],
+        )
+
+    async def spawn(self, arg, timeout_duration=1, has_markdown=True):
+        '''
+        Spawn subprocess and wait `timeout_duration` seconds for error output.
+        If the connection is successful, the instance's `protocol` member will be set
+        to an MpvProtocol for IPC.
+        '''
+        ipc_path = os.path.join(self.plugin.mpv_socket_dir, f"{self.id}")
+
+        try:
+            process, protocol = await create_mpv(
+                self.MPV_ARGS,
+                ipc_path,
+                read_timeout=timeout_duration,
+                loop=self.plugin.nvim.loop
+            )
+            self.protocol = protocol
+
+            protocol.send_command("loadfile", os.path.expanduser(arg))
+            protocol.add_event("property-change", lambda _, __: self.draw_update())
+            # TODO: it would be nice to, if the buffer was unmodified before this, save changes afterward
+            if not has_markdown:
+                protocol.add_event("playback-restart", lambda _, __: self.write_as_markdown(arg))
+            protocol.add_event("error", lambda _, err: self._show_error(err))
+            protocol.add_event("end-file", lambda _, arg: self._on_end_file(arg))
+            protocol.add_event("file-loaded", lambda _, __: self.preamble())
+            protocol.add_event("close", lambda _, __: self.close())
+        except MpvError as e:
+            self.plugin.show_error(e.args[0])
+            if self.id is not None:
+                self.close()
+
+    def _show_error(self, err):
+        additional_info = ""
+        if (property_name := err.get("property-name")) is not None:
+            additional_info = f" to request for property '{property_name}'"
+
+        self.plugin.show_error(
+            f"MPV responded '{err.get('error')}'{additional_info}",
+        )
+
+    def _on_end_file(self, arg):
+        if arg.get("reason") == "error" and (error := arg.get("file_error")):
+            self.plugin.show_error(f"File ended: {error}")
+
+    def preamble(self):
+        self.protocol.observe_property("playback-time")
+        self.protocol.observe_property("pause")
+        self.protocol.observe_property("loop")
+        self.protocol.get_property("duration")
+        self.protocol.get_property("filename")
+        self.protocol.get_property("media-title")
+
+    def close(self):
+        self.plugin.nvim.async_call(self.plugin.remove_mpv_instance, self)
